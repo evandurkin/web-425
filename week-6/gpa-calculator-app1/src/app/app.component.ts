26510604f8399c1651fe5655278a5983bbf8c1eb
Evan Durkin <edurkin@my365.bellevue.edu>
2022-02-09 21:18:51 -0500

    adding exercise 6.3

---
+++ week-6/gpa-calculator-app1/src/app/app.component.ts
@@ -0,0 +1,12 @@
+// Title: Exercise 6.3 - Layouts
+// Author: Evan Durkin
+// Date: February 9, 2022
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {}
